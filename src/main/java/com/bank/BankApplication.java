df63085200b80e9c26315cfbfd1f7c79f9898ede
StarOneId <ttk09112004@gmail.com>
2022-10-16 11:40:00 +0700

    Done Project

--- src/main/java/com/bank/BankApplication.java
+++ src/main/java/com/bank/BankApplication.java
@@ -3,7 +3,7 @@ package com.bank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-
+// Done Project : Banking Application
 @SpringBootApplication
 @EnableAsync
 public class BankApplication {
